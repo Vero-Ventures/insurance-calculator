c0a1c5a652ea757a7a586bcb6e816f6099906eab
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-20 14:37:48 -0700

    Adjust auth tests (#25)

--- testing/tests/login.test.ts
+++ testing/tests/login.test.ts
@@ -1,28 +1,34 @@
-import { test, chromium } from '@playwright/test';
+import { test, chromium, expect } from '@playwright/test';
 import { config } from './config';
 
-test('Login test', async () => {
+// For testing existing email
+const testData = {
+  email: 'jihoonyoon0@gmail.com',
+  password: '123456',
+};
+
+test('Login with registered email and correct password', async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(`${config.BASE_URL}/auth`);
-
-  // Select elements
-  const email = await page.getByText('Email');
-  const password = await page.getByText('Password');
-
-  // TODO: Eventually want to randomly generate email
-  await email?.fill('jyoon72@my.bcit.ca');
-  // TODO: Eventually want to create validation for password
-  await password?.fill('123456');
-
-  // TODO: Maybe make the button text "Sign in" instead of "Login" to stay consistent
-  const signInButton = await page.$('button:has-text("Login")');
-  await signInButton?.click();
 
   try {
+    await page.goto(`${config.BASE_URL}/auth`);
+
+    const email = await page.getByText('Email');
+    const password = await page.getByText('Password');
+
+    await email?.fill(testData.email);
+    await password?.fill(testData.password);
+
+    const signInButton = await page.$('button:has-text("Login")');
+    await signInButton?.click();
     await page.waitForURL('**/landing');
-    console.log('Sign-in test passed');
+    await expect(page).toHaveURL(/.*\/landing/, { timeout: 5000 });
+    console.log('Login with registered email and correct password passed');
   } catch (error) {
-    console.error(`Sign-in test failed, URL: ${page.url()}`);
+    console.error('Login with registered email and correct password failed');
+    throw error;
+  } finally {
+    await browser.close();
   }
 });
--- testing/tests/signup.test.ts
+++ testing/tests/signup.test.ts
@@ -1,33 +1,63 @@
 import { test, chromium } from '@playwright/test';
 import { config } from './config';
 
-test('Signup test', async () => {
-  // Open a browser instance
+// For testing existing email
+const testData = {
+  email: 'jyoon72@my.bcit.ca',
+  password: '12345678',
+};
+
+test('Signup with unregistered email', async () => {
   const browser = await chromium.launch();
-  // Launch a new browser context
-  // const context = await browser.newContext();
-  // Create a new page
   const page = await browser.newPage();
 
-  // Navigate to the URL
-  await page.goto(`${config.BASE_URL}/auth`);
+  try {
+    await page.goto(`${config.BASE_URL}/auth`);
 
-  // Select elements
-  const email = await page.getByText('Email');
-  const password = await page.getByText('Password');
+    const email = await page.getByText('Email');
+    const password = await page.getByText('Password');
 
-  // TODO: Eventually want to randomly generate email
-  await email?.fill('jyoon72@my.bcit.ca');
-  // TODO: Eventually want to create validation for password
-  await password?.fill('123456');
+    // TODO: Eventually want to randomly generate email
+    await email?.fill('jyoon72@my.bcit.ca');
+    // TODO: Eventually want to create validation for password
+    await password?.fill('123456');
 
-  const signupButton = await page.$('button:has-text("Signup")');
-  await signupButton?.click();
+    const signupButton = await page.$('button:has-text("Signup")');
+    await signupButton?.click();
+    // TODO: Update -- I think email verification has been removed?
+    // await page.waitForURL('**/landing');
+    // console.log('Signup test passed');
+  } catch (error) {
+    console.error('Signup test with unregistered email failed');
+    throw error;
+  } finally {
+    await browser.close();
+  }
+});
+
+test('Signup with existing email', async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
   try {
-    await page.waitForURL('**/landing');
-    console.log('Signup test passed');
+    await page.goto(`${config.BASE_URL}/auth`);
+
+    const email = await page.getByText('Email');
+    const password = await page.getByText('Password');
+
+    await email?.fill(testData.email);
+    await password?.fill(testData.password);
+
+    const signupButton = await page.$('button:has-text("Signup")');
+    await signupButton?.click();
+    await page.waitForSelector('text="User already registered"', {
+      state: 'visible',
+    });
+    console.error('Signup test with existing email passed');
   } catch (error) {
-    console.error('Signup test failed');
+    console.log('Signup test with existing email failed');
+    throw error;
+  } finally {
+    await browser.close();
   }
 });
