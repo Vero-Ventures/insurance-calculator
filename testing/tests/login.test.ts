7b1ef62809b6a5ef38578983bf56562e96db72d7
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-21 21:31:54 -0700

    Adjust authentication tests (#25)

--- testing/tests/login.test.ts
+++ testing/tests/login.test.ts
@@ -1,5 +1,5 @@
-import { test, chromium, expect } from '@playwright/test';
-import { config } from './config';
+import { test, expect } from '@playwright/test';
+import { config } from './setup/config';
 
 // For testing existing email
 const testData = {
@@ -7,16 +7,15 @@ const testData = {
   password: '123456',
 };
 
-test('Login with registered email and correct password', async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-
+test('Login with registered email and correct password', async ({
+  browser,
+}) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
   try {
     await page.goto(`${config.BASE_URL}/auth`);
-
     const email = await page.getByText('Email');
     const password = await page.getByText('Password');
-
     await email?.fill(testData.email);
     await password?.fill(testData.password);
 
@@ -28,7 +27,51 @@ test('Login with registered email and correct password', async () => {
   } catch (error) {
     console.error('Login with registered email and correct password failed');
     throw error;
-  } finally {
-    await browser.close();
+  }
+});
+
+test('Login with registered email and incorrect password', async ({
+  browser,
+}) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  try {
+    await page.goto(`${config.BASE_URL}/auth`);
+    const email = await page.getByText('Email');
+    const password = await page.getByText('Password');
+    await email?.fill(testData.email);
+    await password?.fill(testData.password + '1');
+
+    const signInButton = await page.$('button:has-text("Login")');
+    await signInButton?.click();
+    await page.waitForSelector('text="Invalid login credentials"', {
+      state: 'visible',
+    });
+    console.log('Login with registered email and incorrect password passed');
+  } catch (error) {
+    console.error('Login with registered email and incorrect password failed');
+    throw error;
+  }
+});
+
+test('Login with unregistered email and password', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  try {
+    await page.goto(`${config.BASE_URL}/auth`);
+    const email = await page.getByText('Email');
+    const password = await page.getByText('Password');
+    await email?.fill(testData.email + '1');
+    await password?.fill(testData.password);
+
+    const signInButton = await page.$('button:has-text("Login")');
+    await signInButton?.click();
+    await page.waitForSelector('text="Invalid login credentials"', {
+      state: 'visible',
+    });
+    console.log('Login with unregistered email and password passed');
+  } catch (error) {
+    console.error('Login with unregistered email and password failed');
+    throw error;
   }
 });
--- testing/tests/signup.test.ts
+++ testing/tests/signup.test.ts
@@ -1,5 +1,5 @@
-import { test, chromium } from '@playwright/test';
-import { config } from './config';
+import { test } from '@playwright/test';
+import { config } from './setup/config';
 
 // For testing existing email
 const testData = {
@@ -7,44 +7,35 @@ const testData = {
   password: '12345678',
 };
 
-test('Signup with unregistered email', async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+// test('Signup with unregistered email', async ({browser}) => {
+//   const page = await browser.newPage();
+//   try {
+//     await page.goto(`${config.BASE_URL}/auth`);
+//     const email = await page.getByText('Email');
+//     const password = await page.getByText('Password');
+//     // TODO: Eventually want to randomly generate email
+//     await email?.fill('jyoon72@my.bcit.ca');
+//     // TODO: Eventually want to create validation for password
+//     await password?.fill('123456');
 
+//     const signupButton = await page.$('button:has-text("Signup")');
+//     await signupButton?.click();
+//     // TODO: Update -- I think email verification has been removed?
+//     // await page.waitForURL('**/landing');
+//     // console.log('Signup test passed');
+//   } catch (error) {
+//     console.error('Signup test with unregistered email failed');
+//     throw error;
+//   }
+// });
+
+test('Signup with existing email', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
   try {
     await page.goto(`${config.BASE_URL}/auth`);
-
     const email = await page.getByText('Email');
     const password = await page.getByText('Password');
-
-    // TODO: Eventually want to randomly generate email
-    await email?.fill('jyoon72@my.bcit.ca');
-    // TODO: Eventually want to create validation for password
-    await password?.fill('123456');
-
-    const signupButton = await page.$('button:has-text("Signup")');
-    await signupButton?.click();
-    // TODO: Update -- I think email verification has been removed?
-    // await page.waitForURL('**/landing');
-    // console.log('Signup test passed');
-  } catch (error) {
-    console.error('Signup test with unregistered email failed');
-    throw error;
-  } finally {
-    await browser.close();
-  }
-});
-
-test('Signup with existing email', async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-
-  try {
-    await page.goto(`${config.BASE_URL}/auth`);
-
-    const email = await page.getByText('Email');
-    const password = await page.getByText('Password');
-
     await email?.fill(testData.email);
     await password?.fill(testData.password);
 
@@ -57,7 +48,5 @@ test('Signup with existing email', async () => {
   } catch (error) {
     console.log('Signup test with existing email failed');
     throw error;
-  } finally {
-    await browser.close();
   }
 });
