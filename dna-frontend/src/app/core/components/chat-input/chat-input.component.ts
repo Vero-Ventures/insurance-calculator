0f163638fe0a8432ce2d480377b4938b82d7fc6d
nng32 <nng32@my.bcit.ca>
2024-05-23 15:02:17 -0700

    Add prompt field (#91)

---
+++ dna-frontend/src/app/core/components/chat-input/chat-input.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiTextareaModule } from '@taiga-ui/kit';
+
+@Component({
+  selector: 'app-chat-input',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    TuiTextareaModule,
+    TuiButtonModule,
+  ],
+  templateUrl: './chat-input.component.html',
+  styleUrl: './chat-input.component.scss',
+})
+export class ChatInputComponent {
+  promptForm = new FormGroup({
+    prompt: new FormControl(),
+  });
+}
--- dna-frontend/src/app/pages/advisor/advisor.component.ts
+++ dna-frontend/src/app/pages/advisor/advisor.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AdvisorBarComponent } from 'app/core/components/advisor-bar/advisor-bar.component';
+import { ChatInputComponent } from 'app/core/components/chat-input/chat-input.component';
 import { ChatboxComponent } from 'app/core/components/chatbox/chatbox.component';
 
 @Component({
   selector: 'app-advisor',
   standalone: true,
-  imports: [AdvisorBarComponent, ChatboxComponent],
+  imports: [AdvisorBarComponent, ChatboxComponent, ChatInputComponent],
   templateUrl: './advisor.component.html',
   styleUrl: './advisor.component.scss',
 })
