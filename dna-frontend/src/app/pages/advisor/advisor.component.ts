d0c5307262e23848428bfe181198f1352da0d130
nng32 <nng32@my.bcit.ca>
2024-05-23 16:22:04 -0700

    Dynamically fill chat window with message variable (#91)

--- dna-frontend/src/app/pages/advisor/advisor.component.ts
+++ dna-frontend/src/app/pages/advisor/advisor.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { AdvisorBarComponent } from 'app/core/components/advisor-bar/advisor-bar.component';
 import { ChatInputComponent } from 'app/core/components/chat-input/chat-input.component';
@@ -6,8 +7,21 @@ import { ChatboxComponent } from 'app/core/components/chatbox/chatbox.component'
 @Component({
   selector: 'app-advisor',
   standalone: true,
-  imports: [AdvisorBarComponent, ChatboxComponent, ChatInputComponent],
+  imports: [AdvisorBarComponent, ChatboxComponent, ChatInputComponent, NgFor],
   templateUrl: './advisor.component.html',
   styleUrl: './advisor.component.scss',
 })
-export class AdvisorComponent {}
+export class AdvisorComponent {
+  messages = [
+    {
+      side: 'right',
+      author: 'You',
+      contents: 'Question?',
+    },
+    {
+      side: 'left',
+      author: 'Robo-Advisor',
+      contents: 'Answer.',
+    },
+  ];
+}
