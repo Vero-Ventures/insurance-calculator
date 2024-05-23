473237767277b3460cf5182d3e3b0f78c45d62bc
nng32 <nng32@my.bcit.ca>
2024-05-23 13:53:38 -0700

    Create chatbox component (#91)

--- dna-frontend/src/app/pages/advisor/advisor.component.ts
+++ dna-frontend/src/app/pages/advisor/advisor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AdvisorBarComponent } from 'app/core/components/advisor-bar/advisor-bar.component';
+import { ChatboxComponent } from 'app/core/components/chatbox/chatbox.component';
 
 @Component({
   selector: 'app-advisor',
   standalone: true,
-  imports: [AdvisorBarComponent],
+  imports: [AdvisorBarComponent, ChatboxComponent],
   templateUrl: './advisor.component.html',
   styleUrl: './advisor.component.scss',
 })
