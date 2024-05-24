63a7168dc250574cd6e60d6f28889990ff5dad18
nng32 <nng32@my.bcit.ca>
2024-05-24 16:04:53 -0700

    Hook up experimental API (#91)

--- dna-frontend/src/app/pages/advisor/advisor.component.ts
+++ dna-frontend/src/app/pages/advisor/advisor.component.ts
@@ -1,4 +1,5 @@
 import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AdvisorBarComponent } from 'app/core/components/advisor-bar/advisor-bar.component';
 import { ChatInputComponent } from 'app/core/components/chat-input/chat-input.component';
@@ -12,11 +13,20 @@ import { ChatboxComponent } from 'app/core/components/chatbox/chatbox.component'
   styleUrl: './advisor.component.scss',
 })
 export class AdvisorComponent {
-  defaultName = 'You';
+  // queryEndpoint = 'https://insurance-calculator-backend.onrender.com/api/v1/advisor/query';
+  queryEndpoint = 'https://dna-backend-32fw.onrender.com/api/v1/advisor/query';
+
+  defaultUsername = 'You';
+  defaultAdvisorName = 'Robo-Advisor';
   messages: Array<{ side: string; author: string; contents: string }> = [];
+  http: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
 
   addMessage(author: string, contents: string) {
-    const side = author == 'Robo-Advisor' ? 'left' : 'right';
+    const side = author == this.defaultAdvisorName ? 'left' : 'right';
     this.messages.push({
       side: side,
       author: author,
@@ -25,8 +35,22 @@ export class AdvisorComponent {
   }
 
   sendMessage(author: string, contents: string) {
+    if (contents.trim().length == 0) {
+      return;
+    }
+
     this.addMessage(author, contents);
 
-    // TODO: Connect to LLM here and use addMessage when LLM responds
+    this.http
+      .post<ApiResponse>(this.queryEndpoint, {
+        prompt: contents,
+      })
+      .subscribe(value => {
+        console.log(value);
+        this.addMessage(this.defaultAdvisorName, value.data);
+      });
   }
 }
+type ApiResponse = {
+  data: string;
+};
