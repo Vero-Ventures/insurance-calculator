2acbadbde6893f0ac4123a210b381d9134e016b3
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-27 15:09:02 -0700

    Create component tests (#25)

--- dna-frontend/src/app/core/components/pie-chart/pie-chart.component.spec.ts
+++ dna-frontend/src/app/core/components/pie-chart/pie-chart.component.spec.ts
@@ -13,10 +13,36 @@ describe('PieChartComponent', () => {
 
     fixture = TestBed.createComponent(PieChartComponent);
     component = fixture.componentInstance;
+    component.value = [
+      {
+        name: 'name',
+        value: 1,
+      },
+      {
+        name: 'name',
+        value: 2,
+      },
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return the total value', () => {
+    expect(component.total).toBe(
+      component.value.reduce((acc, item) => acc + item.value, 0)
+    );
+  });
+
+  it('should return the names', () => {
+    expect(component.names).toEqual(component.value.map(item => item.name));
+  });
+
+  it('should return the values', () => {
+    expect(component.values).toEqual(
+      component.value.map(item => item.value / component.total)
+    );
   });
 });
