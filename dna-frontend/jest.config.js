deac69b89e9f44b4b3644476550ac4d58be33ea3
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-27 01:30:27 -0700

    Edit config files

--- dna-frontend/jest.config.js
+++ dna-frontend/jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: 'jest-playwright-preset',
-  testMatch: ['**/jest_tests/**.test.ts', '**/jest_tests/**/**.test.ts'],
+  testMatch: [
+    '**/jest_tests/**.test.ts', 
+    '**/jest_tests/**/**.test.ts',
+    'src/**/*.spec.ts'
+  ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  collectCoverage: true,
   testTimeout: 20000,
 };
