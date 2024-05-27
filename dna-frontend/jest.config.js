c8a97e06ec8152ac4686de4badcbda5fb37e8272
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-26 18:10:09 -0700

    Configure Jest

---
+++ dna-frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'jest-playwright-preset',
+    testMatch: [
+      '**/jest_tests/**.test.ts',
+      '**/jest_tests/**/**.test.ts',
+    ],
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    }
+}
