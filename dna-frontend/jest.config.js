5e963c6b9e954a14bf14bd6640fec83d33c57ed7
jyoonbcit <jyoonbcit@users.noreply.github.com>
2024-05-27 01:48:01 +0000

    Apply formatting changes

--- dna-frontend/jest.config.js
+++ dna-frontend/jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    preset: 'jest-playwright-preset',
-    testMatch: [
-      '**/jest_tests/**.test.ts',
-      '**/jest_tests/**/**.test.ts',
-    ],
-    transform: {
-        '^.+\\.ts$': 'ts-jest'
-    },
-    collectCoverage: true
-}
+  preset: 'jest-playwright-preset',
+  testMatch: ['**/jest_tests/**.test.ts', '**/jest_tests/**/**.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverage: true,
+};
