0c3a3ecfa50e80d22bee03c61a8be2b3f8cfdd3a
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-29 01:42:31 -0700

    Add config

--- dna-frontend/playwright.config.ts
+++ dna-frontend/playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   /* 10 second timeout */
   timeout: 20000,
-  testDir: './tests',
+  testDir: './playwright_tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
